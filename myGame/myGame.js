f269157fef9e33db24ddebcb785b8aa6137ebd99
laserquest <amarston.de@mysummitps.org>
2016-10-20 18:10:09 +0000

    update

--- myGame/myGame.js
+++ myGame/myGame.js
@@ -11,36 +11,42 @@ var player;
 var cursors;
 var stars;
 var scoreText;
-var score;
+var score = 0;
+
 
 function preload() {
-    game.load.image('sky', 'assets/fire.jpg');
+    game.load.image('sky', 'assets/whitebag.jpg');
     game.load.image('ground', 'assets/platform.png');
-    game.load.image('star', 'assets/star.png');
-    game.load.spritesheet('dude', 'assets/dude.png', 32, 48);
+    game.load.image('star', 'assets/jugg.jpg');
+    game.load.spritesheet('dude', 'assets/dude.png', 32, 32);
+    
 }
 
 function create() {
     game.add.sprite(0, 0, 'sky');
-
+    
 
 
     platforms = game.add.group();
     platforms.enableBody = true;
-
+    
 
     var ground = platforms.create(0, game.world.height - 64, 'ground');
     ground.scale.setTo(2, 2);
     ground.body.immovable = true;
 
     player = game.add.sprite(32, game.world.height - 150, 'dude');
+    player.scale.setTo(2,2);
     game.physics.arcade.enable(player);
 
     player.body.bounce.y = 0.25;
     player.body.gravity.y = 100;
     player.body.collideWorldBounds = true;
-    player.animations.add('left' [0, 1, 2, 3], 10, true);
-    player.animations.add('right' [5, 6, 7, 8], 10, true);
+    
+    // Change green body box, width, height, x offset, y offset
+    player.body.setSize(11, 10, 10, 18);
+    player.animations.add('left', [0, 1], 10, true);
+    player.animations.add('right', [3, 4], 10, true);
 
     var ledge = platforms.create(-200, 340, 'ground');
     ledge.body.immovable = true;
@@ -62,11 +68,12 @@ function create() {
         var star = stars.create(i * 70, 0, 'star');
         
         //Let gravity do its thing
-        star.body.gravity.y = 6;
+        star.body.gravity.y = 10;
         
         //This just gives each star a slighty random bounce value
         star.body.bounce.y = 0.7 + Math.random() * 0.2;
     }
+    
          game.physics.arcade.collide(star, platforms);
 
     scoreText = game.add.text(16, 16, 'score: 0', { fontSize: '32px', fill: '#000'});
@@ -89,7 +96,7 @@ function update() {
     else {
         player.animations.stop();
 
-        player.frame = 4;
+        player.frame = 2;
     }
 
     if (cursors.up.isDown && player.body.touching.down && hitPlatform) {
@@ -100,7 +107,31 @@ function update() {
     game.physics.arcade.overlap(player, stars, collectStar, null, this);
     
     scoreText.text = "score: " + score;
+    
+    
+    game.debug.body(player);
+    
+    
+    starRespawnTimer += 1; 
+    if(starRespawnTimer > 100){
+    for (var i = 0; i < 50; i++)
+    {
+        //Create a star inside of the 'stars' group
+        var star = stars.create(i * 70, 0, 'star');
+        
+        //Let gravity do its thing
+        star.body.gravity.y = 6;
+        
+        //This just gives each star a slighty random bounce value
+        star.body.bounce.y = 0.7 + Math.random() * 0.2;
+    }
+     starRespawnTimer = 0;   
+    }
+    
+    
 }
+
+var starRespawnTimer = 0;
 
 function collectStar (player, star) {
     star.kill();
